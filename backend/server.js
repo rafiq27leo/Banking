f9d8656fdd66789d9dc1ceafc8420cffe5ffb1c8
rafiq27leo <mdrafiq2707@gmail.com>
2024-08-25 22:07:43 +0530

    Update server.js
    Added new flow changes and routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 
@@ -19,176 +19,326 @@ const PORT = process.env.PORT || 8050;
 
 // MongoDB Connnection
 
-mongoose.connect("mongodb://127.0.0.1:27017/banking").then(() => {
-    console.log("Connected to Database")
-    app.listen(PORT, () => console.log('API is running on http://localhost:8050'));
-
-}).catch((err) =>{
-    console.log("Error while connecting to DB", err)
-});
+mongoose
+  .connect("mongodb://127.0.0.1:27017/banking")
+  .then(() => {
+    console.log("Connected to Database");
+    app.listen(PORT, () =>
+      console.log("API is running on http://localhost:8050")
+    );
+  })
+  .catch((err) => {
+    console.log("Error while connecting to DB", err);
+  });
 
 // Schema
+
 // Users Schema
 const userSchema = mongoose.Schema({
-    name:String,
-    email:String,
-    mobile:Number,
-    password:String
+  name: String,
+  email: String,
+  mobile: Number,
+  password: String,
 });
+
+const transactionSchema = mongoose.Schema(
+  {
+    date: { type: Date, default: Date.now },
+    amount: { type: Number, required: true },
+    description: String,
+    type: { type: String, enum: ["credit", "debit"], required: true },
+  },
+  { _id: false }
+);
 
 // New Account Schema
 const accountSchema = mongoose.Schema({
-  accountNumber:String,
-  fullName:String,
-  fname:String,
-  lname:String,
-  email:String,
-  mobile:Number,
-  password:String,
-  address:String,
-  balance:Number,
-  status:String
+  accountNumber: String,
+  fullName: String,
+  fname: String,
+  lname: String,
+  email: String,
+  mobile: Number,
+  password: String,
+  dob: String,
+  address: String,
+  status: String,
+  balance: { type: Number, required: true, default: 0 },
+  date: String,
+  role:String,
+  transactions: [transactionSchema],
 });
 
+const Account = mongoose.model("Accounts", accountSchema);
 
-
-const transactionsSchema = new mongoose.Schema({
-  date: Date,
-  amount: Number,
-  description: String,
-  type:String
-});
-
-const transactionSchema = new mongoose.Schema({
-  transactions: [transactionsSchema],
-});
-
-const Account = mongoose.model('Accounts', accountSchema);
-
-const User = mongoose.model('User', userSchema);
-
-const Transactions = mongoose.model('Transactions', transactionSchema);
-
+const User = mongoose.model("User", userSchema);
 
 // Routes
 
-app.post('/api/accounts', async (req,res) => {
-    try{
-        const { accountNumber,fullName,fname,lname, mobile,email,password,address,balance,status } = req.body;
-       const hashedPassword = await bcrypt.hash(password.toString(), 10);
-        const newAccount = new Account({accountNumber,fullName,fname,lname,email,password:hashedPassword, mobile,address,balance,status})
-        await newAccount.save();
-        res.status(201).send("Account Registered Successfully");
-    }
-
-    catch(error){
-        console.log("Error while register", error)
-    }
-});
-
-
-app.post("/api/login", async(req,res) =>{
-  try{
-    const { email,password } = req.body;
-    const user = await Account.findOne({email});
+app.post("/api/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await Account.findOne({ email });
     // console.log("Login Data",user)
-    if(user && (await bcrypt.compare(password, user.password))){
-      // console.log("INside Login")
-      res.cookie('authToken', user._id.toString(), { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
-      // console.log(req.cookies.authToken);
-      // res.cookie('userToken', user._id.toString(), {httpOnly:true});
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.cookie("authToken", user._id.toString(), {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      });
       res.status(201).send("User LoggedIn Successfully");
-    }
-    else{
-      // console.log("INside Invalid")
-
+    } else {
       res.status(401).send("Invaild Credentials");
     }
-  }
-  catch(err) {
+  } catch (err) {
     res.status(500).send("Error Registering User");
-  };
-})
-
-app.post("/api/logout", (req,res) => {
-    res.clearCookie('authToken');
-    res.status(200).send("User Logout Successfully");
-});
-
-app.post('/api/register', async (req,res) => {
-  try{
-      const { name,email,mobile,password } = req.body;
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const newUser = new User({name,email,mobile,password:hashedPassword})
-      await newUser.save();
-      res.status(201).send("User Registered Successfully");
-  }
-
-  catch(error){
-      console.log("Error while register", error)
   }
 });
 
-app.get('/api/accounts', async (req,res) => {
-  try{
-      const accounts = await Account.find({});
-      res.status(200).send(accounts);
-  }
-  catch(error){
-      res.status(500).send({message:error.message});
+app.post("/api/logout", (req, res) => {
+  res.clearCookie("authToken");
+  res.status(200).send("User Logout Successfully");
+});
+
+
+
+app.post("/api/register", async (req, res) => {
+  try {
+    const { name, email, mobile, password } = req.body;
+    
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newAccount = new Account({
+      accountNumber:"BS10000000",
+      fullName:name,
+      fname: name,
+      lname: name,
+      mobile:mobile,
+      email:email,
+      password: hashedPassword,
+      dob:"12/12/1990",
+      address:"",
+      balance:0,
+      status:"Active",
+      role:"admin",
+      transactions:[],
+    });
+    // const newUser = new Account({ name, email, mobile, password: hashedPassword });
+    // console.log(newAccount)
+    // return;
+    await newAccount.save();
+    res.status(201).send("User Registered Successfully");
+  } catch (error) {
+    console.log("Error while register", error);
   }
 });
 
-app.put('/api/accounts/:accountNumber', async (req, res) => {
-  try{
-    const { 
-      sender_account,
-      account_number,
+app.post("/api/accounts", async (req, res) => {
+  try {
+    const {
+      accountNumber,
+      fullName,
+      fname,
+      lname,
+      mobile,
+      email,
+      password,
+      dob,
+      address,
       balance,
-      description,
+      status
+    } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password.toString(), 10);
+
+    const transactions = [];
+    transactions.push({
+      type: "credit",
+      amount: balance,
+      description: "Initial Balance Credited",
+      date: new Date(),
+    });
+
+    const newAccount = new Account({
+      accountNumber,
+      fullName,
+      fname,
+      lname,
+      mobile,
+      email,
+      password: hashedPassword,
+      dob,
+      address,
+      balance,
+      status,
+      transactions,
+      role:"user"
+    });
+    // console.log(newAccount);
+
+    await newAccount.save();
+    res.status(201).send("Account Registered Successfully");
+  } catch (error) {
+    console.log("Error while register", error);
+    res.status(500).send("Error while registering account");
+  }
+});
+
+app.get("/api/accounts", async (req, res) => {
+  try {
+    const accounts = await Account.find({});
+    res.status(200).send(accounts);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.post("/api/transfer", async (req, res) => {
+  try {
+    const { sender_account, accountNumber, description, amount, date } =
+      req.body;
+
+    if (!sender_account || !accountNumber || !amount || amount <= 0) {
+      return res.status(400).json({ message: "Invalid input" });
+    }
+
+    const fromAccount = await Account.find({ accountNumber: sender_account });
+    const toAccount = await Account.find({ accountNumber: accountNumber });
+    // console.log(fromAccount)
+
+    if (!fromAccount || !toAccount) throw new Error("Account(s) not found");
+    if (fromAccount.balance < amount) throw new Error("Insufficient funds");
+
+    fromAccount[0].transactions.push({
+      type: "debit",
       amount,
-      date } = req.body;
-    // if (balance >= amount) {
-    //     balance -= amount;  
-    // }
+      description,
+      date: new Date(),
+    });
+    await fromAccount[0].save();
 
-    const transactions = {
-      description:description,
-      amount:amount,
-      date:date,
-      type:"Credit"
-    }
-    console.log(transactions)
-    const { accountNumber } = req.params;
-    const account = await Account.find({accountNumber, accountNumber},
-      {
-        $set: {
-          'transactions.$': transactions // Update the matched transaction
-        }
-      },
-      { new: true, runValidators: true } 
+    fromAccount[0].balance =
+      parseFloat(fromAccount[0].balance) - parseFloat(amount);
+    // console.log(parseFloat(fromAccount[0].balance), " " , fromAccount[0].balance)
+    await Account.findByIdAndUpdate(
+      fromAccount[0]._id,
+      { $set: { balance: fromAccount[0].balance } },
+      { new: true } // Return the updated document
     );
-    if (account) {
-      // console.log(account);
-      
-      console.log("Account updated")
-    } else {
-      res.status(404).json({ message: 'account not found' });
+
+    toAccount[0].transactions.push({
+      type: "credit",
+      amount,
+      description:description,
+      date: new Date(),
+    });
+    await toAccount[0].save();
+    toAccount[0].balance =
+      parseFloat(toAccount[0].balance) + parseFloat(amount);
+    console.log(toAccount[0].balance);
+    await Account.findByIdAndUpdate(
+      toAccount[0]._id,
+      { $set: { balance: toAccount[0].balance } },
+      { new: true } // Return the updated document
+    );
+
+    res.status(200).json({
+      message: "Transfer successful",
+      fromAccountBalance: fromAccount[0].balance,
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+app.post("/api/currency-transfer", async (req, res) => {
+  try {
+    const { sender_account, accountNumber,sender_amount, description, amount, date } =
+      req.body;
+    
+    if (!sender_account || !accountNumber || !amount || amount <= 0) {
+      return res.status(400).json({ message: "Invalid input" });
     }
 
+    const fromAccount = await Account.find({ accountNumber: sender_account });
+    const toAccount = await Account.find({ accountNumber: accountNumber });
+    // console.log(fromAccount)
+    // return
+    if (!fromAccount || !toAccount) throw new Error("Account(s) not found");
+    if (fromAccount.balance < amount) throw new Error("Insufficient funds");
 
+    fromAccount[0].transactions.push({
+      type: "debit",
+      amount: parseFloat(sender_amount),
+      description,
+      date: new Date(),
+    });
+    
+    await fromAccount[0].save();
 
+    fromAccount[0].balance =
+      parseFloat(fromAccount[0].balance) - parseFloat(sender_amount);
+    // console.log(parseFloat(fromAccount[0].balance), " " , fromAccount[0].balance)
+    await Account.findByIdAndUpdate(
+      fromAccount[0]._id,
+      { $set: { balance: fromAccount[0].balance } },
+      { new: true } // Return the updated document
+    );
+
+   
+
+    toAccount[0].transactions.push({
+      type: "credit",
+      description:description,
+      amount: parseFloat(amount),
+      date: new Date(),
+    });
+    await toAccount[0].save();
+
+    console.log("***********hdgjsahgd********", toAccount[0])
+    toAccount[0].balance =
+      parseFloat(toAccount[0].balance) + parseFloat(amount);
+    console.log(toAccount[0].balance);
+    await Account.findByIdAndUpdate(
+      toAccount[0]._id,
+      { $set: { balance: toAccount[0].balance } },
+      { new: true } // Return the updated document
+    );
+    // console.log("*******************", toAccount[0])
+    // return;
+    res.status(200).json({
+      message: "Transfer successful",
+      fromAccountBalance: fromAccount[0].balance,
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
-  catch(err){
-    res.status(500).send({message:err.message});
+});
 
+
+app.get("/api/transactions/:email", async (req, res) => {
+  try {
+    const accounts = await Account.find({ email: req.params.email });
+    res.status(200).send(accounts);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
   }
-  
-  // if (account.balance >= amount) {
-  //   account.balance -= amount;
-  //   // In a real system, you would also update the recipient's account
-  //   res.sendStatus(200);
-  // } else {
-  //   res.status(400).json({ message: 'Insufficient funds' });
-  // }
+});
+
+
+app.get('/api/account', async (req, res) => {
+  try {
+    const { email } = req.query;
+    if (!email) {
+      return res.status(400).send({ message: 'Email parameter is required' });
+    }
+
+    const account = await Account.findOne({ email });
+    if (!account) {
+      return res.status(404).send({ message: 'Account not found' });
+    }
+
+    res.send(account);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: 'Server error' });
+  }
 });
